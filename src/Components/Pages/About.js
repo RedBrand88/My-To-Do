77bd08fc2819293f09f0b4f1d642667b1203ad23
RedBrand88 <brandon@thefoodtruckleague.com>
2020-04-04 09:50:39 -0600

    add about page

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = (props) => {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>v0.1.0</p>
+            <p>
+                This project was created by Brandon Bashein 
+                to stay sharp on his React knowledge and go 
+                deeper into whats happening under the hood
+            </p>
+        </React.Fragment>
+    );
+}
+
+export default About;
